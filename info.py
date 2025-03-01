feae37c86c961f0cfa38ff56f09f788281993f7f
Farook13 <umarulfarook787@gmail.com>
2025-03-01 21:17:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,9 +15,9 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ.get('API_ID', '12618934'))
+API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7857321740:AAHSUfjwO3w6Uffmxm9vCUMl36FtXl5-r6w")
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -27,7 +27,7 @@ PORT = environ.get("PORT", "8000")
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', '📂 <em>File Name</em>: <code>Kᴜᴛᴛᴜ|{file_name}</code> <br><br>🖇 <em>File Size</em>: <code>{file_size}</code> <br><br>❤️‍🔥 <i>Movie Requests</i> - ||@wudixh|| '))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5032034594').split()] 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -40,8 +40,8 @@ FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CUSTOM_FILE_CAPTION}")
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = nviron.get('DATABASE_URI', "mongodb+srv://pcmovies:pcmovies@cluster0.4vv9ebl.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "pcmovies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # INLINE USAGES
