92c9b2ac2a0795d3ada2fae4d56ca24d88258052
Farook13 <umarulfarook787@gmail.com>
2025-03-01 21:32:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', '📂 <em>File Name<
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5032034594').split()] 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in  environ.get('CHANNELS', '-1002407207522').split()] 
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
